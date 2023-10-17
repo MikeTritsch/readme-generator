520f8b8c7a86c4ccd488c83ef35c8bc3834974ac
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-10-17 16:49:52 -0500

    added questions into question array

--- index.js
+++ index.js
@@ -1,14 +1,66 @@
 // TODO: Include packages needed for this application
 // Import file here = require('path to file');
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your app/project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a 2-4 sentence description of your app/project:'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'List any installation instructions (if none, type "N/A"):'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'List all steps needed to successfully use your application:'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Select your desired license here:',
+            choices: ['MIT License', 'Apache 2.0 License', 'GNU GPLv3']
+        },
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'List the names of people (space separated) you worked with to complete this application/project:'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Provide any tests you wrote for this application with instructions on how to run them:'
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Enter your GitHub username:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email addresss:'
+        }
+    ])
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // Call functions from generate markdown
-// Need to import generatemarkdown
 // fs.writeFile(fileName, generatemarkdown(data))
 
 // TODO: Create a function to initialize app
