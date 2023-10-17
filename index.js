96624ea8cb90d62a249a1fd8bb599355758133f5
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-10-17 15:49:47 -0500

    Added starter code

---
+++ index.js
@@ -0,0 +1,23 @@
+// TODO: Include packages needed for this application
+// Import file here = require('path to file');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// Call functions from generate markdown
+// Need to import generatemarkdown
+// fs.writeFile(fileName, generatemarkdown(data))
+
+// TODO: Create a function to initialize app
+// Inquirer questions should be here
+function init() {}
+
+// .then((reponse) => {
+    // writeToFile(README.md, response)
+// })
+
+// Function call to initialize app
+init();
